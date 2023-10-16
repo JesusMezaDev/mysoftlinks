72d024f0e4089aa6ebc121578b56046354f16388
Jesús Meza <jame181185@gmail.com>
2023-10-16 14:05:34 -0700

    Se agrega el favicon y el ícono de la aplicación.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <header>
       <div class="m-3 d-flex justify-content-center">
+        <img src="/shortlink.svg" alt="ShortLink" width="32" height="32" />
         <h3>MyShortLinks</h3>
       </div>
     </header>
