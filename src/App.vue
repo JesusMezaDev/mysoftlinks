efa96fecdc621ac52d64643ce42c3219a6e3d65b
Jesús Meza <jame181185@gmail.com>
2023-10-17 18:27:04 -0700

    Se centra el contenido de la página principal parecido al estilo del buscador de Google.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <SpinnerView />
-  <div class="container">
-    <header>
-      <div class="m-3 d-flex justify-content-center">
-        <img src="/shortlink.svg" alt="ShortLink" width="32" height="32" />
-        <h3>MyShortLinks</h3>
-      </div>
-    </header>
-    <main>
-      <router-view></router-view>
-    </main>
+  <div class="content-middle">
+    <div class="container">
+      <header>
+        <div class="m-3 d-flex justify-content-center">
+          <img src="/shortlink.svg" alt="Ícono de My Short Link" width="32" height="32" />
+          <h3>MyShortLinks</h3>
+        </div>
+      </header>
+      <main>
+        <router-view></router-view>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -18,5 +20,12 @@
 </script>
 
 <style scoped>
-
+  .content-middle {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: calc(100% - 10px);
+    padding: 10px;
+  }
 </style>
