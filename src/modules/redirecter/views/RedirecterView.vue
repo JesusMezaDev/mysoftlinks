839ae6ecedd031b0a3f665f2faf166220c6335db
Jesús Meza <jame181185@gmail.com>
2023-10-17 19:29:39 -0700

    Se agrega el spinner cuando se consulta una url.

--- src/modules/redirecter/views/RedirecterView.vue
+++ src/modules/redirecter/views/RedirecterView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col">
+        <div class="col text-center">
             Redirigiendo...
         </div>
     </div>
@@ -8,18 +8,26 @@
 
 <script setup lang="ts">
     import { onBeforeMount } from 'vue';
+    import { storeToRefs } from 'pinia';
     import { useLinks } from '@/composables/useLinks';
     import { useRoute, useRouter } from 'vue-router';
+    import { linksStore } from '@/stores/links';
+    
+    const { loading } = storeToRefs(linksStore());
     const { getLink } = useLinks();
     const route = useRoute();
     const router = useRouter();
 
     const redirect = async () => {
+        loading.value = true;
         try {
             const link = await getLink(route.params.shortUrl.toString());
             window.open(link, '_self');
         } catch (error) {
             router.push('pagenotfound');
+        }
+        finally {
+            loading.value = false;
         }
     }
 
